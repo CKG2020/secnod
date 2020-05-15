619afac270ffdc025856d5fb5af7e0420be02e1f
easyarch-ckg <2488397825@qq.com>
2020-05-16 00:37:40 +0800

    c++test

---
+++ CppTest/cmake-build-debug/CMakeFiles/CppTest.dir/DependInfo.cmake
@@ -0,0 +1,23 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/ckg/secnod/secnod/CppTest/Father.cpp" "/home/ckg/secnod/secnod/CppTest/cmake-build-debug/CMakeFiles/CppTest.dir/Father.cpp.o"
+  "/home/ckg/secnod/secnod/CppTest/main.cpp" "/home/ckg/secnod/secnod/CppTest/cmake-build-debug/CMakeFiles/CppTest.dir/main.cpp.o"
+  "/home/ckg/secnod/secnod/CppTest/test.cpp" "/home/ckg/secnod/secnod/CppTest/cmake-build-debug/CMakeFiles/CppTest.dir/test.cpp.o"
+  "/home/ckg/secnod/secnod/CppTest/testperson.cpp" "/home/ckg/secnod/secnod/CppTest/cmake-build-debug/CMakeFiles/CppTest.dir/testperson.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CppTest/cmake-build-debug/CMakeFiles/CppTest.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/CppTest.dir/main.cpp.o"
+  "CMakeFiles/CppTest.dir/Father.cpp.o"
+  "CMakeFiles/CppTest.dir/test.cpp.o"
+  "CMakeFiles/CppTest.dir/testperson.cpp.o"
+  "CppTest.pdb"
+  "CppTest"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/CppTest.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
