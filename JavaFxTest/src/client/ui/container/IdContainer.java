690303cc539c5bdd3c438480e3e6e036d1b2d440
easyarch-ckg <2488397825@qq.com>
2020-06-25 23:57:26 +0800

    javaFx

---
+++ JavaFxTest/src/client/ui/container/IdContainer.java
@@ -0,0 +1,7 @@
+package client.ui.container;
+
+public class IdContainer {
+    public static final String RegisterView = "RegisterView";
+    public static final String LoginView = "LoginView";
+    public static final String MainView="MainView";
+}
